27ef58562e0649c81b02bc8e1049c98ee8bdf345
Yoshwa04 <josedmiguel85@gmail.com>
2025-11-11 14:56:02 +0100

    11/11/25

--- logic/math.py
+++ logic/math.py
@@ -1,13 +1,28 @@
 from sympy import symbols, Eq, solve, sympify
 
 class Constants():
+    '''This class just contains some mathematic constants'''
+    
     PI = 3.14159265358979323846
-    E = 2.71 # Euler
-    C = 299792458 # Light Speed
+    E = 2.71 
+    '''Euler'''
+    C = 299792458
+    '''Light Speed'''
     DEGREES_TO_RADIANS = 0.017453292519943295
     RADIANS_TO_DEGREES = 57.29577951308232
+    
+    
+formula_list = {
+    
+}
+
 
 def solve_equation(*eq_given):
+    '''
+        This method solves the given equation/s with the most exactitude possible. 
+        When theres no enough equations for the existing incognites in that equations it just reduces the lenght at its maximum.
+    '''
+    
     try:
         eq_sym = []
         for i in eq_given:
@@ -20,7 +35,8 @@ def solve_equation(*eq_given):
         return solve(eq_sym, *var, dict=True)
     except Exception as e:
         return f"Error: {e}"
-    
+
+
 # res = solve_equation("x = 643/2")
 # x = symbols('x')
 
