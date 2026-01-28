f9d32d7e173d4462e3f2630d62ad8950308446d7
Yoshwa04 <josedmiguel85@gmail.com>
2026-01-28 11:14:41 +0100

    La unica felicitación que no quería recibir, y la única felicitación que recibo

--- provisional_Anima_Eclipse/battle_methods.py
+++ provisional_Anima_Eclipse/battle_methods.py
@@ -1,28 +1,28 @@
 from anima import Anima
-from logic.math_core.solver import give_just_one_solution, solve_equation
+from logic.math_core.solver import *
 from logic.random_utils.probability import chance
 from player import Player
 from dict import arcana_mod_dict, formula_dict, critical_index_dict, stat_inc_dec_dict
 from category import Category
 from technique import Technique
 from type import effectiveness_chart
-from constants import *
+from constants import ELEMENT_STATUS_RESIDUAL_DAMAGE
 
 
 # PROVISIONAL
 
 # el lvl si es el del que has debilitado lo tengo que poner que se pase por parametro.
-def calc_exp(player: Player, anima: Anima, combat_type: float) -> int:
+def calc_exp(player: Player, anima: Anima, foe_anima: Anima, combat_type: float) -> int:
     for confident in player.confidents:
         arcana_lvl = confident.lvl if anima.arcana == confident.arcana else arcana_lvl = 0
     
     arcana_mod = arcana_mod_dict.get(arcana_lvl)
-    object_mod = 1.5 if anima.object == "lucky_egg" else object_mod = 1
+    object_mod = 1.5 if anima.object == "lucky_egg" else object_mod = 1 # el objeto no se como se hará al final, de momento lo dejo como un str y ya
     
-    return int(give_just_one_solution(solve_equation(formula_dict["exp_given"], f"exp_base_given = {anima.exp_base_given}", f"lvl = {anima.lvl}", f"participants = ", f"ally_lvl = ", f"combat_type = {combat_type}", f"object_mod = {object_mod}", f"arcana_mod = {arcana_mod}"), "exp_given"))  
+    return int(give_just_one_solution(solve_equation(formula_dict["exp_given"], f"exp_base_given = {foe_anima.exp_base_given}", f"lvl = {foe_anima.lvl}", f"participants = ", f"ally_lvl = ", f"combat_type = {combat_type}", f"object_mod = {object_mod}", f"arcana_mod = {arcana_mod}"), "exp_given"))  
 
 
-# Esto se guardara en la logica del combate, para poder mandar el mensaje de ¡FALLAS / ESQUIVAS!... etc
+# Esto se guardara en la logica del combate, para poder mandar el mensaje de ¡FALLAS / ESQUIVAS!... y tal
 def calc_hit(anima_atk: Anima, anima_def: Anima, tech: Technique) -> bool:
     raw = give_just_one_solution(solve_equation(formula_dict["hit_chance"], f"move_accuracy = {tech.accuracy}", f"attacker_accuracy = {stat_inc_dec_dict[anima_atk.stats_inc_dec["acc"]]}", f"defender_evasion = {stat_inc_dec_dict[anima_def.stats_inc_dec["eva"]]}"), "hit_chance")
     
@@ -32,7 +32,7 @@ def calc_hit(anima_atk: Anima, anima_def: Anima, tech: Technique) -> bool:
 def calc_crit(anima: Anima) -> bool:
     return chance(critical_index_dict[anima.crit_index])
 
-# Se pasa el critico como boleano como parametro porque se tiene que saber fuera si ha sido critico, asi puedo mostrar un mensaje informando tipo ¡CRITICO! y tal 
+# Se pasa el critico en boleano como parametro porque se tiene que saber fuera si ha sido critico, asi puedo mostrar un mensaje informando tipo ¡CRITICO! y tal 
 def calc_direct_damage(anima_atk: Anima, anima_def: Anima, tech: Technique, critical: bool) -> int:
     stab = _calc_stab(anima_atk, tech)
     atk = _calc_atk(anima_atk, tech)
